2e14ebe90a2d0378e0382cbc83d6d83a64d4e90c
Brian Simonton <bsimonton78@gmail.com>
2019-06-19 17:59:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street){
+  
+  if (street >= 42) {return street - 42}
+  else {return 42 - street}
+  
+  
+}
+
+
+function distanceFromHqInFeet(street){
+  
+  return (distanceFromHqInBlocks(street) * 264 )
+  
+  
+  
+}
+
+
+
+function distanceTravelledInFeet(start, end){
+  
+  if (start > end){return (start - end) * 264} 
+  else {return (end - start) * 264} 
+  
+  
+}
+
+
+function calculatesFarePrice(start, end){
+  
+  const total_distance = distanceTravelledInFeet(start, end)
+  
+  let fare; 
+  
+  switch(true){
+    case (total_distance <= 400) : fare = 0
+    break;
+    case (total_distance > 400 && total_distance <= 2000) : fare = ((total_distance -400) * 0.02)
+    break;
+    case (total_distance > 2000 && total_distance <= 2500) : fare = 25
+    break;
+    default : return "cannot travel that far"
+    }
+  
+  return fare; 
+  
+  
+}
