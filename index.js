48f93a7ab6f1436773c7460c01236a5bb6332bd1
Brian Simonton <bsimonton78@gmail.com>
2019-06-19 17:36:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(){
+function distanceFromHqInBlocks(street){
   
   if (street >= 42) {return street - 42}
-  
+  else {return 42 - street}
   
   
 }
